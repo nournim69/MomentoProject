3771a01de1e7eb647143091ad46a12c294203ce4
Nour Nimry <nournim98@gmail.com>
2026-03-27 12:52:53 +0300

    update nour 2

--- src/App.jsx
+++ src/App.jsx
@@ -275,7 +275,7 @@ function App() {
 							? userRole === ROLE_MASTER_ADMIN
 								? "אתה מחובר כ-Master Admin."
 								: "אתה מחובר כ-Event Admin."
-							: " כמשתמש רגיל ללא הרשאות מנהל."}
+							: "התחברת כמשתמש רגיל ללא הרשאות מנהל."}
 					</p>
 
 					{userRole === ROLE_MASTER_ADMIN && (
